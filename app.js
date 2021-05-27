bf3bad0ecf7ce36aeb2897b7eb008ebed11495c7
itmaxxx <dmitrievmaks77@gmail.com>
2021-05-27 21:55:24 +0300

    handle regex url's

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 require('dotenv').config();
 const http = require('http');
 const mongoose = require('mongoose');
+const Emitter = require('./classes/EventEmmiter');
 
 const port = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  if (req.method !== 'GET') {
-    res.end(
-      JSON.stringify({ error: http.STATUS_CODES[405] })
-    );
-  } else {
-    if (req.url === '/') {
-      res.end(`<h1>Hello World</h1>`);
-    }
-    if (req.url === '/hello') {
-      res.end(`<h1>Hello</h1>`);
-    }
-  }
+const app = new Emitter();
 
-  res.end(
-    JSON.stringify({ error: http.STATUS_CODES[404] })
-  );
+app.on(/^\/files\/(\d{0,9})$/, function (req, res) {
+  console.log('/files/[id]', this.matches);
+
+  res.end(`/files/${this.matches[1]}`);
+});
+
+app.on(/^\/user\/(\d{0,9})\/(.+)$/, function (req, res) {
+  console.log('/user/[id]/[string]', this.matches);
+
+  res.end(`/user/${this.matches[1]}/${this.matches[2]}`);
+});
+
+app.on(/^\/pdf\/(\d{0,9})$/, function (req, res) {
+  console.log('/pdf/[id]', this.matches);
+
+  res.end(`/pdf/${this.matches[1]}`);
+});
+
+app.on(/^\/$/, function (req, res) {
+  console.log('/');
+
+  res.end('Hi there');
+});
+
+app.on(/.+/, function (req, res) {
+  res.end('*');
+});
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET':
+      app.emit(req.url, req, res);
+      break;
+    default:
+      res.end(
+        JSON.stringify({ error: http.STATUS_CODES[404] })
+      );
+      break;
+  }
 });
 
 async function start() {
